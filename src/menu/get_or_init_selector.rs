fd420dbab68787e4b4f264b8547c4bfef609142d
zphrs <46908127+zphyrj@users.noreply.github.com>
2024-04-08 01:45:24 -0700

    added explaination for expect statement

--- src/menu/get_or_init_selector.rs
+++ src/menu/get_or_init_selector.rs
@@ -1,6 +1,7 @@
 #[macro_export]
 macro_rules! get_or_init_selector {
     ($sel:expr,$query:literal) => {
-        $sel.get_or_init(|| Selector::parse($query).expect("Selector is valid"))
+        $sel.get_or_init(|| Selector::parse($query)
+        .expect("Selector is valid")) // I use expect here because all queries are hardcoded and therefore should always be valid
     };
 }
