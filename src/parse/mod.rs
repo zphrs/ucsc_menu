db51a4dd712b6d3dd96ab74b82d9d2bc85269ade
zphrs <46908127+zphyrj@users.noreply.github.com>
2024-04-20 05:50:42 -0700

    Added function to clean up names which have extra whitespace, only copying the string when necessary

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,9 +1,11 @@
-mod menu_page;
 mod error;
+mod menu_page;
 pub use error::Error;
 mod location_page;
+mod remove_excess_whitespace;
 mod static_selector;
 mod text_from_selection;
 
 pub use location_page::LocationMeta;
 pub use location_page::Locations;
+pub use remove_excess_whitespace::remove_excess_whitespace;
